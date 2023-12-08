752ce9f9c41fde677fce27bb4c97da0aeda2a563
Max Fortun <max@maxf.net>
2023-12-07 20:57:58 -0500

    updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/ash -e
 
-[ -n $DPSRV_DOMAIN ] || exit
-[ -n $DPSRV_REGION ] || exit
-[ -n $DPSRV_NODE ] || exit
-host dns-main.$DPSRV_DOMAIN | grep -q ${DPSRV_REGION}-${DPSRV_NODE}.$DPSRV_DOMAIN || exit
+[ -n $DPSRV_DOMAIN ] || exit 0
+[ -n $DPSRV_REGION ] || exit 0
+[ -n $DPSRV_NODE ] || exit 0
+host dns-main.$DPSRV_DOMAIN | grep -q ${DPSRV_REGION}-${DPSRV_NODE}.$DPSRV_DOMAIN || exit 0
 
 cd $(dirname $0)
 WD=$(pwd)
